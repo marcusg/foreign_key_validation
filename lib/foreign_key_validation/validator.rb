52dae896a82e17f6e113028a0555576baba9c6fb
Marcus Geißler <marcus3006@gmail.com>
2014-09-30 10:48:09 +0200

    use private methods in validator init

--- lib/foreign_key_validation/validator.rb
+++ lib/foreign_key_validation/validator.rb
@@ -7,11 +7,11 @@ module ForeignKeyValidation
 
     def initialize(klass, opt={})
       self.klass                 = klass
-      self.validate_against      = (opt[:on] || DEFAULT_VALIDATE_AGAINST).to_s
+      self.validate_against      = find_validate_against(opt[:on])
       self.reflections           = klass.reflect_on_all_associations(:belongs_to)
       self.reflection_names      = reflections.map(&:name).map(&:to_s)
-      self.validate_against_key  = reflections.select {|r| r.name.to_s == validate_against}.first.try(:foreign_key)
-      self.validate_with         = ((Array(opt[:with]).map(&:to_s) if opt[:with]) || reflection_names).reject {|n| n == validate_against}
+      self.validate_against_key  = find_validate_against_key
+      self.validate_with         = find_validate_with(opt[:with])
     end
 
     def check
@@ -22,6 +22,20 @@ module ForeignKeyValidation
 
     def filter_method_name
       "validate_foreign_keys_on_#{validate_against}"
+    end
+
+    private
+
+    def find_validate_against(opt_on)
+      (opt_on || DEFAULT_VALIDATE_AGAINST).to_s
+    end
+
+    def find_validate_with(opt_with)
+      ((Array(opt_with).map(&:to_s) if opt_with) || reflection_names).reject {|n| n == validate_against}
+    end
+
+    def find_validate_against_key
+      reflections.select {|r| r.name.to_s == validate_against}.first.try(:foreign_key)
     end
 
   end
