b7c96f869c5dd7825c593e403b5c36215622b907
Marcus Geißler <marcus3006@gmail.com>
2014-10-07 10:23:05 +0200

    use getter instead of initialize assignments

--- lib/foreign_key_validation/collector.rb
+++ lib/foreign_key_validation/collector.rb
@@ -1,17 +1,12 @@
 module ForeignKeyValidation
 
   class Collector
-    attr_accessor :klass, :validate_against, :reflections, :reflection_names, :validate_against_key, :validate_with
+    attr_accessor :options
 
     DEFAULT_VALIDATE_AGAINST = :user
 
     def initialize(opt={})
-      self.klass                 = opt[:klass]
-      self.validate_against      = find_validate_against(opt[:on])
-      self.reflections           = klass.reflect_on_all_associations(:belongs_to)
-      self.reflection_names      = reflections.map(&:name).map(&:to_s)
-      self.validate_against_key  = find_validate_against_key
-      self.validate_with         = find_validate_with(opt[:with])
+      self.options = opt
     end
 
     def check!
@@ -21,18 +16,28 @@ module ForeignKeyValidation
       true
     end
 
-    private
-
-    def find_validate_against(on)
-      (on || DEFAULT_VALIDATE_AGAINST).to_s
+    def klass
+      @klass ||= options[:klass]
     end
 
-    def find_validate_with(with)
-      ((Array(with).map(&:to_s) if with) || reflection_names).reject {|n| n == validate_against}
+    def validate_against
+      @validate_against ||= (options[:on] || DEFAULT_VALIDATE_AGAINST).to_s
     end
 
-    def find_validate_against_key
-      reflections.select {|r| r.name.to_s == validate_against}.first.try(:foreign_key)
+    def validate_with
+      @validate_with ||= ((Array(options[:with]).map(&:to_s) if options[:with]) || reflection_names).reject {|n| n == validate_against}
+    end
+
+    def reflections
+      @reflections ||= klass.reflect_on_all_associations(:belongs_to)
+    end
+
+    def reflection_names
+      @reflection_names ||= reflections.map {|r| r.name.to_s }
+    end
+
+    def validate_against_key
+      @validate_against_key ||= reflections.select {|r| r.name.to_s == validate_against}.first.try(:foreign_key)
     end
 
   end
