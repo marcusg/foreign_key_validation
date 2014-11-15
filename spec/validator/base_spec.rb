222eeea5d02746d729b7563a54b2623cb283a16a
Marcus Geißler <marcus3006@gmail.com>
2014-11-15 12:21:21 +0100

    use test doubles instead of structs

--- spec/validator/base_spec.rb
+++ spec/validator/base_spec.rb
@@ -4,32 +4,37 @@ describe ForeignKeyValidation::Validator do
 
   let(:user) { User.create }
   let(:other_user) { User.create }
+  let(:collector) { double(ForeignKeyValidation::Collector) }
 
   describe ".new" do
+
+    let(:project) { double(Project) }
 
     subject { ForeignKeyValidation::Validator }
 
     it "initializes new validator" do
-      expect(subject.new(double(ForeignKeyValidation::Collector), double(Project)))
-        .to be_instance_of ForeignKeyValidation::Validator
+      expect(subject.new(collector, project)).to be_instance_of ForeignKeyValidation::Validator
     end
 
   end
 
   describe "#validate" do
+    let(:valid_issue) { Issue.create(user: user, project: Project.create(user: user)) }
+    let(:invalid_issue) { Issue.create(user: user, project: Project.create(user: other_user)) }
+
+    before do
+      allow(collector).to receive(:validate_against_key).and_return :user_id
+      allow(collector).to receive(:validate_with).and_return [:project]
+    end
 
     subject { ForeignKeyValidation::Validator }
 
     it "creates no validations if object is valid" do
-      object = Issue.create(user: user, project: Project.create(user: user))
-      collector = OpenStruct.new(validate_against_key: :user_id, validate_with: [:project])
-      expect(subject.new(collector, object).validate).to be false
+      expect(subject.new(collector, valid_issue).validate).to be false
     end
 
     it "creates validations if object is invalid" do
-      object = Issue.create(user: user, project: Project.create(user: other_user))
-      collector = OpenStruct.new(validate_against_key: :user_id, validate_with: [:project])
-      expect(subject.new(collector, object).validate).to be true
+      expect(subject.new(collector, invalid_issue).validate).to be true
     end
 
   end
