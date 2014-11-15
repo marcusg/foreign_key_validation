50783d020cde194a376a4116d7fd90192a978e38
Marcus Geißler <marcus3006@gmail.com>
2014-11-15 15:55:06 +0100

    added some collector specs

--- spec/collector/base_spec.rb
+++ spec/collector/base_spec.rb
@@ -14,6 +14,27 @@ describe ForeignKeyValidation::Collector do
       expect{subject.new(klass: Dummy)}.to raise_error(ForeignKeyValidation::Errors::NoReleationFoundError)
     end
 
+    describe "instance methods" do
+      subject { ForeignKeyValidation::Collector.new(klass: Issue) }
+
+      it "provides access to #klass" do
+        expect(subject.klass).to be Issue
+      end
+
+      it "provides access to #validate_with" do
+        expect(subject.validate_with).to eq ["project"]
+      end
+
+      it "provides access to #validate_against_key" do
+        expect(subject.validate_against_key).to eq "user_id"
+      end
+
+      it "provides access to #validate_against" do
+        expect(subject.validate_against).to eq "user"
+      end
+
+    end
+
   end
 
 end
